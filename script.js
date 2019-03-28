7b766cf11f82b8da2247603ce45fc32b42730692
Felipe Rodrigues <filpgame@gmail.com>
2019-03-28 18:01:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(window).on('load', function () {
-    
+    var description = $('#banner-content > div > div > h3')
+    var preSaleContent = "A pré venda para clientes cartão Elo das novas datas de São Paulo e Rio de Janeiro acabou."
+    if(description.html().indexOf(preSaleContent) >= 0) {
+        setTimeout(function(){
+            location.reload()
+        }, 500);
+    }
 });
