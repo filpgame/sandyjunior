93739a1468d0013fdb5ef1d2d90cfb84a6dccc84
Felipe Rodrigues <filpgame@gmail.com>
2019-03-29 03:48:17 +0000

    change reload condition

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(window).on('load', function () {
-    var description = $('#banner-content > div > div > h3')
-    var preSaleContent = "A pré venda para clientes cartão Elo das novas datas de São Paulo e Rio de Janeiro acabou."
-    if(description.html().indexOf(preSaleContent) >= 0) {
-        setTimeout(function(){
+    var sectorList = $('#sectorList')
+    var preSaleContent = "Esgotado"
+    if (sectorList.html().indexOf(preSaleContent) < 3) {
+        setTimeout(function () {
             location.reload()
         }, 500);
+    } else {
+        //TODO GRITAR alguma coisa
     }
 });
